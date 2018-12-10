97b5a78ecd6ae3399de265c3c985af38f8d18ecf
JaniceGJY <gjy.janicecarol@gmail.com>
2018-12-10 17:11:50 -0500

    Commented ForecastTask and Add Pressure and Temperature Display

--- app/src/main/java/com/ja/getdevicelocation/ForecastTask.java
+++ app/src/main/java/com/ja/getdevicelocation/ForecastTask.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.util.Log;
+import android.widget.Toast;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
@@ -13,66 +14,90 @@ import java.util.List;
 
 
 
-    public class ForecastTask extends AsyncTask<String, Void, String[]> {
-            Context mainContext;
-            Class activityClass;
 
+/**
+ * author: Jiaying Guo
+ * This class is for making cityName forecast API calls concurrently and starting activities generically, mainly for swipe to
+ * view weather forecast info on a daily basis.
+ */
+public class ForecastTask extends AsyncTask<String, Void, String[]> {
+    Context mainContext;
+    Class activityClass;
 
-        public ForecastTask(Context mainContext, Class activityClass){
+    /**
+     * Constructor takes in the Context and Activity Class to initialize corresponding instance variables for starting an intent
+     * @param mainContext
+     * @param activityClass
+     */
+    public ForecastTask(Context mainContext, Class activityClass){
             this.mainContext=mainContext;
             this.activityClass=activityClass;
 
-        }
+    }
 
+        /**
+         * Take in a stream of string arguments, the 1st argv is the cityName forecast URL,
+         * 2nd one is the ith day integer
+         * @param urls
+         * @return a reponse string array
+         */
         @Override
         protected String[] doInBackground(String... urls) {
             ForecastAPI forecastAPI = new ForecastAPI();
-           // String url=forecastAPI.createURL(Double.parseDouble(geos[0]),Double.parseDouble(geos[1]));
             String[] response=new String[3];
             URL forecastURL;
 
             try {
-
                 forecastURL = new URL(urls[0]);
                 response[0]=forecastAPI.makeAPICall(forecastURL);
                 response[1]=urls[1];
                 response[2]=urls[0];
 
-
-
             } catch (MalformedURLException e) {
-
                 e.printStackTrace();
             } catch (IOException e) {
-
                 e.printStackTrace();
             }
 
             return response;
         }
 
-
+        /**
+         * Take in a string array of response, the 1st element is the forecast json response, 2nd one is the ith day integer, 3rd one is
+         * the cityName forecast URL
+         * send the information to the activityClass view
+         * @param response
+         */
         @Override
         protected void onPostExecute(String[] response) {
-            int i=Integer.parseInt(response[1]);
-            ForecastAPI forecastAPI = new ForecastAPI();
+            if(response == null || response[0] == null || response[1] == null || response[0].length()==0 || response[1].length()==0){
+                Toast.makeText(mainContext, "Sorry, cannot get your location's forecast!", Toast.LENGTH_LONG).show();
+            }
 
-            List<DailyWeather> res =forecastAPI.getGeoForecast(response[0]);
+            // Otherwise send the information to the searchInfo view and start a searchInfo activity
+            else {
+                int i = Integer.parseInt(response[1]);
+                ForecastAPI forecastAPI = new ForecastAPI();
 
-            Intent intent = new Intent(mainContext, activityClass);
+                // Get a list of 4 day forecast Daily Weather objects
+                List<DailyWeather> res = forecastAPI.getGeoForecast(response[0]);
+                Intent intent = new Intent(mainContext, activityClass);
+                intent.putExtra("city", res.get(i).getCity());
+                intent.putExtra("pressure", "pressure \n" + res.get(i).getPressure());
+                intent.putExtra("humid", "humid \n" + res.get(i).getHumid());
+                intent.putExtra("mainWeather", res.get(i).getDiscription());
 
-            intent.putExtra("city",res.get(i).getCity());
-            intent.putExtra("pressure",res.get(i).getPressure());
-            intent.putExtra("humid",res.get(i).getHumid());
-            intent.putExtra("mainWeather",res.get(i).getDiscription());
-            double temp=res.get(i).getTemMin()/2+res.get(i).getTemMax()/2;
-            temp=(double)(Math.round(temp*100)/100.0);
-            intent.putExtra("temp",String.valueOf(temp)+" °C");
-            intent.putExtra("forecastURL",response[2]);
-            mainContext.startActivity(intent);
+                // Calculate the average temperature with max Temp and min Temp
+                double temp = res.get(i).getTemMin() / 2 + res.get(i).getTemMax() / 2;
+                temp = (double) (Math.round(temp * 100) / 100.0);
+                intent.putExtra("temp", String.valueOf(temp) + " °C");
+
+                // Send the forecast URL to next view
+                intent.putExtra("forecastURL", response[2]);
+                mainContext.startActivity(intent);
 
 
-
+            }
 
         }
     }
